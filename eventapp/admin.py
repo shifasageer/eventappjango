cfec39f55390acb3b345b86aa94a39bb15351f8e
shifasageer <shifasageer2207@gmail.com>
2023-09-13 14:28:49 +0530

    Event app
    Event registration app created

---
+++ eventapp/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Event
+from .models import Applicant
+
+# Register your models here.
+
+
+admin.site.register(Event)
+
+admin.site.register(Applicant)
---
+++ eventapp/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import Applicant
+
+
+class Applicantform(forms.ModelForm):
+    class Meta:
+        model = Applicant
+        fields = ['full_name','email_id','phone']
